3d5ecb77e883c6ae40ba891ea052b51d7715b290
Andoni Christou <andonilc71@gmail.com>
2023-10-31 23:05:24 -0400

    successfully incorporated unbeatable AI

--- script.js
+++ script.js
@@ -7,19 +7,30 @@ const gameBoard = (function () {
   const getSpots = () => {
     return spots;
   }
-  const gameboard = ['','','','','','', '','',''];
+  const gameboard = [
+    {e: '', i:0},
+    {e: '', i:1},
+    {e: '', i:2},
+    {e: '', i:3},
+    {e: '', i:4},
+    {e: '', i:5},
+    {e: '', i:6},
+    {e: '', i:7},
+    {e: '', i:8},
+
+  ];
 
   const getBoard = () => {
     return gameboard;
   }
   
   const setBoard = (i, val) => {
-    gameboard[i] = val;
+    gameboard[i]["e"] = val;
   }
 
   const render = () => {
     gameboard.forEach((e, index) => {
-      document.querySelector('[data-index=' + CSS.escape(index) + ']').textContent = e;
+      document.querySelector('[data-index=' + CSS.escape(index) + ']').textContent = e["e"];
     })
   }
 
@@ -55,37 +66,57 @@ const game = (function() {
   //game mode buttons
   const humanButton = document.querySelector('.human');
   const aiButton = document.querySelector('.ai');
-
+  const selector = document.querySelector('.selector');
   const gameModeButtons = [humanButton, aiButton];
-
+  const difficulties = document.querySelectorAll('.difficulty');
+  console.log(difficulties)
   //toggle game mode
   humanButton.addEventListener('click', (e) => {
     humanButton.classList.add('selected');
     aiButton.classList.remove('selected');
     playerTwoContainer.style.visibility = "visible";
+    playerTwoInput.style.visibility = "visible"
+    selector.style.visibility = "hidden"
+    restart();
   })
 
   aiButton.addEventListener('click', (e) => {
     aiButton.classList.add('selected');
     humanButton.classList.remove('selected');
     playerTwoContainer.style.visibility = "hidden";
-    playerTwoInput.style.visibility = "hidden"
+    playerTwoInput.style.visibility = "hidden";
+    selector.style.visibility = "visible"
     // input still shows even when do visibility hidden on safari.... (display:none, .remove() similar story)
+    restart();
   })
+
+  selector.addEventListener('change', () => {
+    restart();
+  })
+
+
 
 
   const createPlayers = () => {
     _playerOne.name = playerOneInput.value;
-    _playerTwo.name = playerTwoInput.value;
-    
-    playerArray = [_playerOne, _playerTwo];
     if(_playerOne.name == "") {
       _playerOne.name = "Player One (X)"
     }
+    if(humanButton.classList.contains('selected')) {
+      
+    _playerTwo.name = playerTwoInput.value;
+    
+    playerArray = [_playerOne, _playerTwo];
+  
     if(_playerTwo.name == "") {
       _playerTwo.name = "Player Two (O)"
     }
     console.log(_playerOne)
+    } else if (aiButton.classList.contains('selected')) {
+      _playerTwo.name = "AI";
+      playerArray = [_playerOne, _playerTwo];
+    }
+    
   }
   
 
@@ -110,40 +141,56 @@ const game = (function() {
       createPlayers();
       playerOneInput.readOnly = true;
       playerTwoInput.readOnly = true;
+      
+      
       startBtn.remove();
       gameForm.append(restartBtn);
-      handleClick();
+      if (humanButton.classList.contains('selected')) {
+        handleClick();
+      }
+
+      else if (aiButton.classList.contains('selected')) {
+        if (selector.value=="easy"){
+          handleClickAiRandMode();
+        } else if (selector.value == "unbeatable") {
+          handleClickAiMinimax();
+        }
+        
+      }
     })
   }
 
   const restart = () => {
-    restartBtn.addEventListener('click', (event) => {
-      for (let i=0; i<gameBoard.getBoard().length; i++) {
-        gameBoard.setBoard(i, "");
-      }
-      gameBoard.render();
+    for (let i=0; i<gameBoard.getBoard().length; i++) {
+      gameBoard.setBoard(i, "");
+    }
+    gameBoard.render();
 
-      winnerMessage.textContent = "";
-      gameForm.appendChild(startBtn);
-      restartBtn.remove();
+    winnerMessage.textContent = "";
+    gameForm.appendChild(startBtn);
+    restartBtn.remove();
 
-      playerArray.forEach(player => {
-        player.name = ""
-      })
+    playerArray.forEach(player => {
+      player.name = ""
+    })
 
-      playerInputArray.forEach(playerInput => {
-        playerInput.value = "";
-        playerInput.readOnly = false;
-      })
+    playerInputArray.forEach(playerInput => {
+      playerInput.value = "";
+      playerInput.readOnly = false;
+    })
 
-      turn = 0;
-      numOfTurns = 0;
+    turn = 0;
+    numOfTurns = 0;
 
-      gameBoard.getSpots().forEach((spot, index) => {
-        spot.removeEventListener('click', update);
-        spot.removeEventListener('click', checkAll);
-      })
-     })
+    gameBoard.getSpots().forEach((spot, index) => {
+      spot.removeEventListener('click', update);
+      spot.removeEventListener('click', updateAiRandMode);
+      spot.removeEventListener('click', updateAiMinimax);
+    })
+  }
+
+  const restartListen = () => {
+    restartBtn.addEventListener('click', restart)
   }
 
   const update = (e) => {
@@ -154,15 +201,136 @@ const game = (function() {
         gameBoard.render();
         turn = 1;
         numOfTurns++;
+        displayWinner();
       } else {
         gameBoard.setBoard(spotIndex, playerArray[1].mark);
         gameBoard.render();
         turn = 0;
         numOfTurns++;
+        displayWinner();
       }
     }
-    
   }
+
+  const emptySpots = (board) => {
+    return Array.from(board).filter((spot) => spot.innerText == "-")
+  }
+
+  const emptyElements = (board) => {
+    return board.filter((e) => ((e["e"] != "X") && (e["e"]!="O") && (e["e"]!="")));
+  }
+
+
+  const aiRand = () => {
+    return emptySpots(gameBoard.getSpots())[Math.floor(Math.random() * emptySpots(gameBoard.getSpots()).length)].getAttribute("data-index");
+  }
+
+  const minimax = (currBoard, player) => {
+    let availCells = [];
+
+    //collect array of the available cells
+    emptyElements(currBoard).forEach((spot) => {
+      availCells.push(spot);
+    })
+
+    //this is the terminal checker, i.e. returning a score based on who wins
+    if (check(currBoard, _playerOne)) {
+      return {score:-1};
+    } else if (check(currBoard, _playerTwo)) {
+      return {score: 1};
+    } else if (availCells.length === 0) {
+      return {score: 0};
+    }
+
+    let moves = [];
+
+    // goes through available cells. for each one, makes a move object.
+    for (let i=0; i<availCells.length; i++) {
+      let move = {};
+      move.index = availCells[i]["i"];
+
+      //fills gameboard with the player marking that cell
+      currBoard[availCells[i]["i"]]["e"] = player.mark;
+      
+      //if that player is AI, then it recursively calls minimax with Human player.
+      // this means that the turn switches to the human and it tries to optimize
+      if (player == _playerTwo) {
+        let result = minimax(currBoard, _playerOne);
+        move.score = result.score;
+      } else if (player == _playerOne) {
+        let result = minimax(currBoard, _playerTwo)
+        move.score = result.score;
+      }
+
+      currBoard[availCells[i]["i"]]["e"] = "-";
+      
+      moves.push(move);
+    }
+
+    let bestMoveIndex;
+
+//depending on which player's turn it is, the best move will be the smallest value (if analyzing human), or the largest value (if analyzing AI)
+    if (player == _playerTwo) {
+      bestScore = -10000;
+      for (let i=0; i<moves.length; i++) {
+        if (moves[i].score > bestScore) {
+          bestMoveIndex = i;
+          bestScore = moves[i].score;
+        }
+      }
+    } else if (player == _playerOne) {
+      bestScore = 10000;
+      for (let i=0; i<moves.length; i++) {
+        if (moves[i].score < bestScore) {
+          bestMoveIndex = i;
+          bestScore = moves[i].score;
+        }
+      }
+    }
+
+    //returns the best-move object
+    return moves[bestMoveIndex];
+  }
+
+
+
+
+  const updateAiRandMode = (e) => {
+    let spotIndex = e.target.getAttribute('data-index');
+    if (e.target.textContent != 'X' && e.target.textContent != 'O' && winnerMessage.textContent == "") {
+      if (turn == 0) {
+        gameBoard.setBoard(spotIndex, playerArray[0].mark);
+        gameBoard.render();
+        displayWinner();
+        numOfTurns++;
+        console.log(emptySpots(gameBoard.getSpots()))
+        gameBoard.setBoard(aiRand(), "O");
+        numOfTurns++;
+        gameBoard.render();
+        displayWinner();
+      } 
+    }
+  }
+
+
+
+  const updateAiMinimax = (e) => {
+    let spotIndex = e.target.getAttribute('data-index');
+    if (e.target.textContent != 'X' && e.target.textContent != 'O' && winnerMessage.textContent == "") {
+      if (turn == 0) {
+        gameBoard.setBoard(spotIndex, playerArray[0].mark);
+        gameBoard.render();
+        displayWinner();
+        numOfTurns++;
+        console.log(minimax(gameBoard.getBoard(), _playerTwo))
+        gameBoard.setBoard(minimax(gameBoard.getBoard(), _playerTwo).index, "O");
+        gameBoard.render();
+        displayWinner();
+        numOfTurns++;
+      } 
+    }
+  }
+  
 
   const winningCombinations = [
     [0, 1, 2],
@@ -175,37 +343,58 @@ const game = (function() {
     [2, 4, 6],
   ];
 
-  const check = (player) => {
+  const check = (board, player) => {
+    let winnerFound = false;
     winningCombinations.forEach(e => {
       const [a,b,c] = e;
-      console.log([a,b,c])
-      console.log('sdfsd')
-      if (gameBoard.getBoard()[a]==player.mark && gameBoard.getBoard()[a] == gameBoard.getBoard()[b] && gameBoard.getBoard()[b] == gameBoard.getBoard()[c]) {
-        winnerMessage.textContent = "Winner is " + player.name + "!";
+      if (board[a]["e"]==player.mark && board[a]["e"] == board[b]["e"] && board[b]["e"] == board[c]["e"]) {
+        winnerFound = true;
+        return winnerFound;
         
-      } else if (numOfTurns == 9 && winnerMessage.textContent == "") {
-          winnerMessage.textContent = "Tie";
-        }
+      } 
       
     })
+    return winnerFound;
   }
 
-  const checkAll = () => {
-    playerArray.forEach(check)
+  
+
+  const displayWinner = () => {
+    if (check(gameBoard.getBoard(), _playerOne)) {
+      winnerMessage.textContent = "Winner is " + _playerOne.name + "!";
+    } else if (check(gameBoard.getBoard(), _playerTwo)) {
+      winnerMessage.textContent = "Winner is " + _playerTwo.name + "!";
+    } else if (numOfTurns == 9) {
+      winnerMessage.textContent = "Tie"
+    }
   }
 
   const handleClick = () => {
     gameBoard.getSpots().forEach((spot, index) => {
       spot.addEventListener('click', update);
-      spot.addEventListener('click', checkAll)
+      
     })
     
   }
 
+  const handleClickAiRandMode = () => {
+    gameBoard.getSpots().forEach((spot, index) => {
+      spot.addEventListener('click', updateAiRandMode);
+      spot.addEventListener('click', displayWinner)
+    })
+  }
+
+  const handleClickAiMinimax = () => {
+    gameBoard.getSpots().forEach((spot, index) => {
+      spot.addEventListener('click', updateAiMinimax);
+      spot.addEventListener('click', displayWinner);
+    })
+  }
+
   return {
-    start, restart
+    start, restartListen, check, emptySpots
   }
 })();
 
 game.start();
-game.restart();
+game.restartListen();
